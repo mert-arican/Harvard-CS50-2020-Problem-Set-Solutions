a5ffe77c85d6299b319af801603d8791dff209c3
Mert Arıcan <45623751+mert-arican@users.noreply.github.com>
2020-01-19 01:29:05 +0300

    Update dictionary.c

--- Problem Set 5-Trie/dictionary.c
+++ Problem Set 5-Trie/dictionary.c
@@ -21,6 +21,7 @@ node;
 
 // Represents a trie
 node *root;
+
 int count = 0;
 
 // Loads dictionary into memory, returning true if successful else false
@@ -65,7 +66,6 @@ bool load(const char *dictionary)
                 for (int j = 0; j < N; j++) {
                     new_row->children[j] = NULL;
                 }
-                //                printf("id added: %d\n", id);
                 new_row->is_word = false;
                 new_row->parent = source;
                 temp = new_row;
@@ -96,15 +96,11 @@ bool check(const char *word)
     node *point = root;
     for (int i = 0; i < strlen(word); i++) {
         int hash = tolower(word[i]) % 26;
-        //        if word[i] == '''
         if (!isalnum(word[i])) { hash = 26; }
         point = point->children[hash];
         if (point == NULL) {
             return false;
         }
-    }
-    if (point->is_word) {
-        //        printf("%s AHA PİÇ", word);
     }
     return point->is_word;
 }
@@ -125,9 +121,7 @@ bool unload(void)
         if (!hasChild) {
             if (current->parent != NULL) {
                 for (int i = 0; i < N; i++) {
-                    // Why the heck free(current) doesn't work?
                     if (current == current->parent->children[i]) {
-                        //                        printf("id removed: %d\n", current->parent->children[i]->id);
                         current->parent->children[i] = NULL;
                         free(current->parent->children[i]);
                     }
@@ -137,6 +131,5 @@ bool unload(void)
         }
     }
     free(root);
-    //    printf("MERABALAR AQ!\n %d---%d", appCount, delCount);
     return true;
 }
